557558812cbdf84805a88e65166a3812ce9c92d3
tramacore <138532257+tramacore@users.noreply.github.com>
2024-08-04 02:23:56 +0300

    Delete killster directory

--- killster/utils/complete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-file=sample.txt
-if test -f "$file";then
-echo "Те повезло еще раз" >>  "$file"
-else
-touch "$file"
-echo "Те повезло " >> "$file"
-fi
--- killster/utils/delete.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-killFile=assets/exampleFile1.md
-echo "был удален файл $killFile"
-rm  -rf "$killFile"
